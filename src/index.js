fbbb7b2c6b358cdeacdca47ec45d6cd644732822
jmartinez-uv <jmartinez@unitedvirtualities.com>
2020-02-11 15:34:55 -0700

    EndPoint

--- src/index.js
+++ src/index.js
@@ -5,6 +5,16 @@ app.get('/',function(req,res){
 	res.send('Hello World!');
 })
 
+app.get('/users', function(req, res){
+	//console.log(req.params.id)
+	res.json({
+		user: "juan",
+		email: 'j@gmail.com',
+	})
+})
+
+
+
 app.listen(3000,function(){
 	console.log("server is running");
 })
