81bd9b7e60cbafa13b769d49cf9fa17f56978429
jmartinez-uv <jmartinez@unitedvirtualities.com>
2020-02-23 12:58:32 -0700

    step 3 post

--- src/index.js
+++ src/index.js
@@ -1,5 +1,7 @@
 const express = require('express');
+const bodyParser = require('body-parser');
 const app =  express();
+app.use(bodyParser.json);
 
 const users = [];
 
@@ -10,9 +12,18 @@ app.get('/',function(req,res){
 app.get('/users', function(req, res){
 	//console.log(req.params.id)
 	res.send(users);
-})
+});
 
+app.post('/users', function(req,res){
+	users.push({id:0});
+	res.json(users);
+});
 
+app.get('/users/:id', function(req, res){
+	//console.log(req.params.id)
+	const user = users.find(user => user.id == req.param.id);
+	res.json(users);
+});
 
 app.listen(3000,function(){
 	console.log("server is running");
